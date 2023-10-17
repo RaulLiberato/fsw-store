e75aaf4b1e5875b0974d299a4c47395cd4ac7959
Raul Liberato <ctt.raul.liberato@gmail.com>
2023-10-17 15:50:13 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,45 @@
 import { Card } from "./card";
 import { Button } from "./button";
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 
 const Header = () => {
     return(
         <Card className="flex justify-between p-[1.875rem] items-center">
-            <Button size="icon" variant="outline">
-                <MenuIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+
+                <SheetContent  side="left">
+                    <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
+
+                    <div className="mt-2 flex flex-col gap-2">
+                        <Button variant="outline" className="w-full justify-start gap-2">
+                            <LogInIcon size={16}/>
+                            Fazer Login
+                        </Button>
+
+                        <Button variant="outline" className="w-full justify-start gap-2">
+                            <HomeIcon size={16}/>
+                            Início
+                        </Button>
+
+                        <Button variant="outline" className="w-full justify-start gap-2">
+                            <PercentIcon size={16}/>
+                            Ofertas
+                        </Button>
+
+                        <Button variant="outline" className="w-full justify-start gap-2">
+                            <ListOrderedIcon size={16}/>
+                            Catálogo
+                        </Button>
+                    </div>
+                </SheetContent>
+            </Sheet>
 
             <h1 className="font-semibold text-lg"><span className="text-primary">FWS</span> Store</h1>
             
